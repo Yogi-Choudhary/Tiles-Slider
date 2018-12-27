5e6b2bf46a5115dc46fada5196a8d74932844955
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2018-12-27 08:29:19 +0530

    initial

---
+++ script.js
@@ -0,0 +1,18 @@
+(function ($) {
+    $(function () {
+  
+      /**
+       * Test 1
+       */
+      $('#yogi').tilesSlider();
+  
+      /**
+       * Test 2
+       */
+      $('#yogi2').tilesSlider({
+        delay: 700,
+        effect: 'chaotic'
+      });
+  
+    });
+  })(jQuery);
